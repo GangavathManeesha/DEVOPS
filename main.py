8eb30a14b27218aa9344ed42de2207b99cd89fa8
GangavathManeesha <maneeshagangavath@gmail.com>
2025-03-21 12:56:08 +0530

    main.py

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ import streamlit as st
 
 st.title('Hello World')
 st.write('This is a simple Streamlit app.')
+st.button("Reset", type="primary")
+if st.button("Say hello"):
+    st.write("Why hello there")
+else:
+    st.write("Goodbye")
 
+if st.button("Aloha", type="tertiary"):
+    st.write("Ciao")
