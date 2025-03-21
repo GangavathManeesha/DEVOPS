61d58dd8f1c57ea2ec7e0c6beb7a9781b6485f85
GangavathManeesha <maneeshagangavath@gmail.com>
2025-03-21 13:04:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import streamlit as st
 
-st.title('Hello World')
-st.write('This is a simple Streamlit app.')
-st.button("Reset", type="primary")
-if st.button("Say hello"):
-    st.write("Why hello there")
-else:
-    st.write("Goodbye")
+name = st.text_input("Enter your name", "John Doe")
+password = st.text_input("Enter your password", type="password")
 
-if st.button("Aloha", type="tertiary"):
-    st.write("Ciao")
+
+if st.button("Submit"):
+   if len(password) < 8:
+       st.warning("Password must be at least 8 characters long")
+
+
+   st.success("Password is valid")
+
